a70b3f1c4a5b0228eaada371f42da9fbd8b91f47
Егоров Евгений <eegorov@lanit.ru>
2025-03-14 16:53:20 +0300

    Refactoring

--- main.tf
+++ main.tf
@@ -2,41 +2,42 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-    } 
-  } 
+    }
+  }
 }
 
 resource "yandex_vpc_security_group" "sg" {
-  network_id = var.network_id 
-  name   = var.name
+  network_id  = var.network_id
+  name        = var.name
+  description = try(var.description, null)
 }
 
 resource "yandex_vpc_security_group_rule" "sgr_egress" {
-  count = try(length(var.egress) ,0)
+  count = try(length(var.egress), 0)
 
   security_group_binding = yandex_vpc_security_group.sg.id
-      direction = "egress"
-      protocol = var.egress[count.index].protocol
-      from_port = try(var.egress[count.index].from_port, -1)
-      port = try(var.egress[count.index].port, -1)
-      to_port = try(var.egress[count.index].to_port, -1)
-      v4_cidr_blocks =try(var.egress[count.index].v4_cidr_blocks,[])
-      security_group_id = try(var.egress[count.index].security_group_id, -1)
-      predefined_target = try(var.egress[count.index].predefined_target, null)
-      labels            = {}
+  direction              = "egress"
+  protocol               = var.egress[count.index].protocol
+  from_port              = try(var.egress[count.index].from_port, -1)
+  port                   = try(var.egress[count.index].port, -1)
+  to_port                = try(var.egress[count.index].to_port, -1)
+  v4_cidr_blocks         = try(var.egress[count.index].v4_cidr_blocks, [])
+  security_group_id      = try(var.egress[count.index].security_group_id, -1)
+  predefined_target      = try(var.egress[count.index].predefined_target, null)
+  labels                 = {}
 }
 
 resource "yandex_vpc_security_group_rule" "sgr_ingres" {
-  count = try(length(var.ingress) ,0)
+  count = try(length(var.ingress), 0)
 
   security_group_binding = yandex_vpc_security_group.sg.id
-      direction = "ingress"
-      protocol = var.ingress[count.index].protocol
-      from_port = try(var.ingress[count.index].from_port, -1)
-      port = try(var.ingress[count.index].port, -1)
-      to_port = try(var.ingress[count.index].to_port, -1)
-      v4_cidr_blocks =try(var.ingress[count.index].v4_cidr_blocks,[])
-      security_group_id = try(var.ingress[count.index].security_group_id, -1)
-      predefined_target = try(var.ingress[count.index].predefined_target, null)
-      labels            = {}
+  direction              = "ingress"
+  protocol               = var.ingress[count.index].protocol
+  from_port              = try(var.ingress[count.index].from_port, -1)
+  port                   = try(var.ingress[count.index].port, -1)
+  to_port                = try(var.ingress[count.index].to_port, -1)
+  v4_cidr_blocks         = try(var.ingress[count.index].v4_cidr_blocks, [])
+  security_group_id      = try(var.ingress[count.index].security_group_id, -1)
+  predefined_target      = try(var.ingress[count.index].predefined_target, null)
+  labels                 = {}
 }
--- variables.tf
+++ variables.tf
@@ -1,42 +1,49 @@
 variable "network_id" {
-  description = "The id of target security group which rule belongs to"
+  description = "ID of the network this security group belongs to"
   type        = string
   nullable    = false
 }
 
 
 variable "name" {
-  description = "The id of target security group which rule belongs to"
+  description = "Name of the security group"
   type        = string
   nullable    = false
 }
 
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
 variable "ingress" {
   description = "A list of ingress rules"
-  type        = list(object({
-    protocol = string
-    from_port = optional(number)
-    port = optional(number)
-    to_port = optional(number)
-    v4_cidr_blocks = optional(list(string))
+  type = list(object({
+    protocol          = string
+    from_port         = optional(number)
+    port              = optional(number)
+    to_port           = optional(number)
+    v4_cidr_blocks    = optional(list(string))
     security_group_id = optional(string)
     predefined_target = optional(string)
   }))
-  nullable    = true
-  default     = []
+  nullable = true
+  default  = []
 }
 
 variable "egress" {
   description = "A list of egress rules"
-  type        = list(object({
-    protocol = string
-    from_port = optional(number)
-    port = optional(number)
-    to_port = optional(number)
-    v4_cidr_blocks = optional(list(string))
+  type = list(object({
+    protocol          = string
+    from_port         = optional(number)
+    port              = optional(number)
+    to_port           = optional(number)
+    v4_cidr_blocks    = optional(list(string))
     security_group_id = optional(string)
     predefined_target = optional(string)
   }))
-  nullable    = true
-  default     = []
+  nullable = true
+  default  = []
 }
